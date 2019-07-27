aa95405ba06d6623ab2e398e24a14804a7248df0
Pillár <Miroslav.Pillar@senacor.com>
2019-07-27 10:15:33 +0200

    connecting to backend

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -10,73 +10,73 @@ class Signin extends React.Component {
 	}
 
 	onEmailChange = (event) => {
-		this.setState({signInEmail: event.target.value})
+		this.setState({ signInEmail: event.target.value })
 	}
 
 	onPasswordChange = (event) => {
-		this.setState({signInPassword: event.target.value})
+		this.setState({ signInPassword: event.target.value })
 	}
 
 	onSubmitSignIn = () => {
-		fetch('https://immense-inlet-60412.herokuapp.com/signin/', {
+		fetch('https://guarded-reaches-10517.herokuapp.com/signin', {
 			method: 'post',
-			headers: {'Content-Type': 'application/json'},
+			headers: { 'Content-Type': 'application/json' },
 			body: JSON.stringify({
 				email: this.state.signInEmail,
 				password: this.state.signInPassword
 			})
 		})
-		.then(response => response.json())
-		.then(user => {
-			if (user.id) {
-				this.props.loadUser(user)
-				this.props.onRouteChange('home');
-			}
-		})
+			.then(response => response.json())
+			.then(user => {
+				if (user.id) {
+					this.props.loadUser(user)
+					this.props.onRouteChange('home');
+				}
+			})
 	}
 
 	render() {
 		const { onRouteChange } = this.props;
 		return (
-		<article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-			<main className="pa4 black-80">
-			  <div className="measure">
-			    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-			      <legend className="f2 fw6 ph0 mh0">Sign In</legend>
-			      <div className="mt3">
-			        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-			        <input
-			        	className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-			        	type="email"
-			        	name="email-address"
-			        	id="email-address"
-			        	onChange={this.onEmailChange}
-			        />
-			      </div>
-			      <div className="mv3">
-			        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-			        <input
-			        	className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-			        	type="password"
-			        	name="password"
-			        	id="password"
-			        	onChange={this.onPasswordChange}
-			        />
-			      </div>
-			    </fieldset>
-			    <div className="">
-			      <input
-			      	onClick={this.onSubmitSignIn}
-			      	className="z-1 b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in"
-			      	/>
-			    </div>
-			    <p className='f6'>or</p>
-			    <div className="lh-copy mt3">
-			      <p onClick={() => onRouteChange('register')} className="f6 fw6 underline link dim black db pointer">Register</p>
-			    </div>
-			  </div>
-			</main>
-		</article>
+			<article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+				<main className="pa4 black-80">
+					<div className="measure">
+						<fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+							<legend className="f2 fw6 ph0 mh0">Sign In</legend>
+							<div className="mt3">
+								<label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+								<input
+									className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+									type="email"
+									name="email-address"
+									id="email-address"
+									onChange={this.onEmailChange}
+								/>
+							</div>
+							<div className="mv3">
+								<label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+								<input
+									className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+									type="password"
+									name="password"
+									id="password"
+									onChange={this.onPasswordChange}
+								/>
+							</div>
+						</fieldset>
+						<div className="">
+							<input
+								onClick={this.onSubmitSignIn}
+								className="z-1 b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in"
+							/>
+						</div>
+						<p className='f6'>or</p>
+						<div className="lh-copy mt3">
+							<p onClick={() => onRouteChange('register')} className="f6 fw6 underline link dim black db pointer">Register</p>
+						</div>
+					</div>
+				</main>
+			</article>
 		);
 	}
 }
